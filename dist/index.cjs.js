305b2452a9e96688215b0438c1c28c31d82b2b76
Yaroslav Vinnik <yaroslav.vinnik27@gmail.com>
2021-10-12 14:38:01 +0300

    fix icon

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -860,16 +860,9 @@ var Icon$e = function (props) {
 
 var Icon$d = function (props) {
     return (React__default["default"].createElement(Svg, __assign({ viewBox: "0 0 24 24" }, props),
-        React__default["default"].createElement("g", { "clip-path": "url(#clip0)" },
-            React__default["default"].createElement("circle", { cx: "12", cy: "12", r: "10", fill: "#1263F1" }),
-            React__default["default"].createElement("mask", { id: "mask0", maskUnits: "userSpaceOnUse", x: "9", y: "1", width: "16", height: "23" },
-                React__default["default"].createElement("path", { d: "M12.75 11.25L9.75 21.75L11.25 24L20.25 23.25L23.25 15L24.75 6.75L15 1.5L12.75 8.25V11.25Z", fill: "#F93B5D" })),
-            React__default["default"].createElement("g", { mask: "url(#mask0)" },
-                React__default["default"].createElement("circle", { cx: "12", cy: "12", r: "10", fill: "#F93B5D" })),
-            React__default["default"].createElement("path", { d: "M6 14.3143L15.7714 0L13.2 10.2H18.3429L8.57143 24L11.1429 14.3143H6Z", fill: "white" })),
-        React__default["default"].createElement("defs", null,
-            React__default["default"].createElement("clipPath", { id: "clip0" },
-                React__default["default"].createElement("rect", { width: "24", height: "24", fill: "white" })))));
+        React__default["default"].createElement("circle", { cx: "12", cy: "12", r: "10", fill: "#1263F1" }),
+        React__default["default"].createElement("path", { d: "M9.75195 21.7462L12.7508 11.25V8.25004L14.7104 2.37146C18.9167 3.55273 22.0008 7.41622 22.0008 12C22.0008 17.5229 17.5237 22 12.0008 22C11.2276 22 10.4748 21.9123 9.75195 21.7462Z", fill: "#F93B5D" }),
+        React__default["default"].createElement("path", { d: "M6 14.3143L15.7714 0L13.2 10.2H18.3429L8.57143 24L11.1429 14.3143H6Z", fill: "white" })));
 };
 
 var Icon$c = function (props) {
