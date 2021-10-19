7fc14fcc3ec42c2222ba5843a2fbb8c7a181e2aa
andrii_kfront <andriy.k@lineir.me>
2021-10-19 16:21:37 +0300

    add icons

--- dist/components/Svg/index.d.ts
+++ dist/components/Svg/index.d.ts
@@ -76,5 +76,6 @@ export { default as HammerIcon } from './Icons/Hammer';
 export { default as FavoriteFullIcon } from './Icons/FavoriteFull';
 export { default as FavoriteEmptyIcon } from './Icons/FavoriteEmpty';
 export { default as ShareIcon } from './Icons/Share';
+export { default as ViewIcon } from './Icons/View';
 export { default as Svg } from "./Svg";
 export type { SvgProps } from "./types";
