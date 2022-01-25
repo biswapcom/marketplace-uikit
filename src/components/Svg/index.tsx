4039d1f7ac60ff8a0b193eb00a9730468a7fa225
vladmbs <vlad.m@lineir.me>
2022-01-25 14:44:56 +0200

    add bag icon

--- src/components/Svg/index.tsx
+++ src/components/Svg/index.tsx
@@ -87,5 +87,6 @@ export {default as BinanceUsdIcon} from './Icons/BinanceUsd'
 export {default as UsdtIcon} from './Icons/USDT'
 export {default as RangeThumbIcon} from './Icons/SliderThumb'
 export {default as ContractIcon} from './Icons/Contract'
+export {default as BagIcon} from './Icons/Bag'
 export { default as Svg } from "./Svg";
 export type { SvgProps } from "./types";
