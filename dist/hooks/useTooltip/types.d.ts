c0f27419a2b268d458ecba542976d80aeb455d92
anesbrechoo <anesbrechoo@mem.es>
2022-06-14 16:31:11 +0300

    build

--- dist/hooks/useTooltip/types.d.ts
+++ dist/hooks/useTooltip/types.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="react" />
 import { Placement, Padding } from "@popperjs/core";
 export interface TooltipRefs {
     targetRef: React.Dispatch<React.SetStateAction<HTMLElement | null>>;
